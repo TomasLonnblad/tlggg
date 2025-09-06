a72e96f52aeb9c67099d6169e9b039ba3e264b38
TomasLonnblad <tomaslonnblad@gmail.com>
2025-09-06 14:38:17 +0200

    x

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <h1 className="bg-indigo-500 text-7xl font-extrabold text-center mt-10">About</h1>
+}
+
+export default AboutPage
---
+++ src/app/work/page.tsx
@@ -0,0 +1,5 @@
+const WorkPage = () => {
+  return <h1 className="text-6xl font-extrabold text-center mt-10">Work</h1>
+}
+
+export default WorkPage
